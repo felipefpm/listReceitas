19eb62c7424879626007d7b20326a608cc05355d
Felipe Pinto <felipepinto.fpm@gmail.com>
2020-03-20 22:59:52 -0300

    Ajustes e criaçao dos cards de exibiçao

--- frontend/src/Pages/HomeList/index.jsx
+++ frontend/src/Pages/HomeList/index.jsx
@@ -10,7 +10,77 @@ function HomeList({ match }) {
       <Main />
       <div className="body-container">
         <div className="inter-content">
-          <div className="contet">Texto</div>
+          <div className="contet">
+            <ul>
+              <li className="card-content">
+                <img
+                  src="https://img.elo7.com.br/product/zoom/258B7CB/adesivo-parede-restaurante-prato-feito-comida-caseira-lenha-adesivo-restaurante-fritas-salada.jpg"
+                  alt=""
+                />
+                <footer>
+                  <strong>Receita 01</strong>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Etiam tristique urna nec massa egestas sodales. Nunc et
+                    ipsum at urna molestie porttitor suscipit eget tellus. Morbi
+                    aliquet magna sit amet lectus convallis.
+                  </p>
+                  <button>Detalhes</button>
+                </footer>
+              </li>
+
+              <li className="card-content">
+                <img
+                  src="https://img.elo7.com.br/product/zoom/258B7CB/adesivo-parede-restaurante-prato-feito-comida-caseira-lenha-adesivo-restaurante-fritas-salada.jpg"
+                  alt=""
+                />
+                <footer>
+                  <strong>Receita 01</strong>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Etiam tristique urna nec massa egestas sodales. Nunc et
+                    ipsum at urna molestie porttitor suscipit eget tellus. Morbi
+                    aliquet magna sit amet lectus convallis.
+                  </p>
+                  <button>Detalhes</button>
+                </footer>
+              </li>
+
+              <li className="card-content">
+                <img
+                  src="https://img.elo7.com.br/product/zoom/258B7CB/adesivo-parede-restaurante-prato-feito-comida-caseira-lenha-adesivo-restaurante-fritas-salada.jpg"
+                  alt=""
+                />
+                <footer>
+                  <strong>Receita 01</strong>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Etiam tristique urna nec massa egestas sodales. Nunc et
+                    ipsum at urna molestie porttitor suscipit eget tellus. Morbi
+                    aliquet magna sit amet lectus convallis.
+                  </p>
+                  <button>Detalhes</button>
+                </footer>
+              </li>
+
+              <li className="card-content">
+                <img
+                  src="https://img.elo7.com.br/product/zoom/258B7CB/adesivo-parede-restaurante-prato-feito-comida-caseira-lenha-adesivo-restaurante-fritas-salada.jpg"
+                  alt=""
+                />
+                <footer>
+                  <strong>Receita 01</strong>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Etiam tristique urna nec massa egestas sodales. Nunc et
+                    ipsum at urna molestie porttitor suscipit eget tellus. Morbi
+                    aliquet magna sit amet lectus convallis.
+                  </p>
+                  <button>Detalhes</button>
+                </footer>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </>
